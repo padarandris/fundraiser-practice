d3e64573cf86f521f188411dadd4b38b61e0896e
Pádár András <padarandris@gmail.com>
2021-01-12 09:57:47 +0100

    First commit

---
+++ src/test/java/hu/progmasters/fundraiser/FundraiserApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.progmasters.fundraiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundraiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
